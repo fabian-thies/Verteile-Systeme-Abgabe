3bf6c27a6721cd4c3422bd8dfc03392e87f51bd2
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-02-12 20:40:20 +0100

    feat: add blazor server

---
+++ ChatApp/Program.cs
@@ -0,0 +1,29 @@
+using ChatApp.Components;
+using ChatApp.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();

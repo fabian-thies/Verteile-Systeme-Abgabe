52fbaaab8b5f74031fbc12eb87bb9d0ea8f86b19
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-02-25 22:46:29 +0100

    login

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -1,5 +1,9 @@
 using ChatApp.Components;
+using ChatApp.Data;
 using ChatApp.Hubs;
+using ChatApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,13 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("logs/ChatApp.log", rollingInterval: RollingInterval.Day)
     .Enrich.WithProperty("Application", "ChatApp")
     .CreateLogger();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
@@ -26,6 +37,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()

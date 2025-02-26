0573753e335e6cb9b36d4a474ae94e890c014bfa
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-02-27 00:10:43 +0100

    add logout

---
+++ ChatFileApp/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatFileApp.Pages.Account;
+
+public class Logout : PageModel
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public Logout(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+        SignOutUserAsync().GetAwaiter().GetResult();
+    }
+
+    public void OnGet()
+    {
+        Response.Redirect("/");
+    }
+
+    private async Task SignOutUserAsync()
+    {
+        await _signInManager.SignOutAsync();
+    }
+}

887cab5c2b7c1d60969bcc2f55da716249d8ad40
fabianthies <44522183+fabian-thies@users.noreply.github.com>
2025-03-04 23:16:03 +0100

    new project

--- ChatFileApp/postcss.config.js
+++
@@ -1,3 +0,0 @@
-﻿export default {
-    plugins: ["@tailwindcss/postcss"],
-};
--- WhiteboardPlugin/wwwroot/js/whiteboard.js
+++
@@ -1,2 +0,0 @@
-﻿// English comment: Whiteboard plugin JavaScript logic
-console.log("Whiteboard plugin script loaded.");
